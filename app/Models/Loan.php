f014fd46bed17f5c02e25c7be8646210507aa11e
Gus <gustavoocanto@gmail.com>
2021-11-20 10:28:53 +0800

    add seeder

--- app/Models/Loan.php
+++ app/Models/Loan.php
@@ -14,6 +14,7 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 /**
  * @property int $id
  * @property string $uuid
+ * @property float $lent_amount
  * @property Carbon $created_at
  * @property Carbon $updated_at
  * @property Carbon $deleted_at
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,18 +1,61 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Database\Seeders;
 
+use App\Models\Installment;
+use App\Models\Loan;
+use App\Models\User;
+use Carbon\CarbonImmutable;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
-    public function run()
+    private User $user;
+    private CarbonImmutable $now;
+
+    public function __construct()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->now = CarbonImmutable::parse('2022-01-01')->startOfDay();
+
+        $this->user = User::factory()->create(['name' => 'Gus', 'email' => 'gus@loans.local']);
+    }
+
+    public function run(): void
+    {
+        $this->createLoan('fdb7c9cf-c20c-478f-8def-596229afec0c', ['description' => 'Loan 001', 'lent_amount' => 100]);
+        $this->createLoan('d77c1bbe-fec1-419f-a574-1c149c4c59cc', ['description' => 'Loan 002', 'lent_amount' => 200]);
+        $this->createLoan('7aca94e6-ed0e-4ed5-adbc-005812dc2734', ['description' => 'Loan 003', 'lent_amount' => 300]);
+    }
+
+    private function createLoan(string $uuid, array $attrs) : void
+    {
+        /** @var Loan $loan */
+        $loan = Loan::factory()->create(\array_merge([
+            'uuid' => $uuid,
+            'user_id' => $this->user->id
+        ], $attrs));
+
+        $this->createInstallments($loan);
+    }
+
+    private function createInstallments(Loan $loan): void
+    {
+        $amount = $loan->lent_amount / 2; //we assume same amount installments.
+
+        Installment::factory()->create([
+            'loan_id' => $loan->id,
+            'due_date' => $this->now->toDateString(),
+            'due_amount' => $amount,
+            'paid_at' => $this->now,
+        ]);
+
+        Installment::factory()->create([
+            'loan_id' => $loan->id,
+            'due_date' => $this->now->addWeek()->toDateString(),
+            'due_amount' => $amount,
+            'paid_at' => null,
+        ]);
     }
 }
